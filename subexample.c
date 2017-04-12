bfccb76f2dda256d1f73a283f5edf2c83c1f5c12
Francesco Antoniazzi <francesco.antoniazzi1991@gmail.com>
2017-04-12 09:36:31 +0200

    gitignore for documentation

--- subexample.c
+++ subexample.c
@@ -1,7 +1,7 @@
 /*
  * subexample.c
  * 
- * Copyright 2017 Francesco Antoniazzi <francesco@ArcesAlienware>
+ * Copyright 2017 Francesco Antoniazzi <francesco.antoniazzi@unibo.it>
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
